805a438fac14dfa8bd336f91d2201d60a8af1e7c
laupwing <loc_strict@hotmail.com>
2024-04-07 15:33:57 +0200

    Add Navbar component to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
-import Image from "next/image"
+import Navbar from "@/components/global/navbar"
 
 export default function Home() {
-    return <main></main>
+    return (
+        <main>
+            <Navbar />
+        </main>
+    )
 }
